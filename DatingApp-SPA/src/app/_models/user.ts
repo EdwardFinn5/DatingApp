6d59f98e3a5b0237511ddd5904caa2be87eeb6c8
Ed Finn <ed@iowacollegefoundation.org>
2020-06-23 14:14:13 -0500

    End of session 9

---
+++ DatingApp-SPA/src/app/_models/user.ts
@@ -0,0 +1,18 @@
+import { Photo } from './photo';
+
+export interface User {
+    id: number;
+    username: string;
+    knownAs: string;
+    age: number;
+    classYear: string;
+    created: Date;
+    lastActive: Date;
+    photoUrl: string;
+    hometown: string;
+    college: string;
+    major?: string;
+    experiences?: string;
+    lookingFor?: string;
+    photos?: Photo[];
+}
