cdd81ff134ebf96df12245b6ac3b681e6beeffc5
Ed Finn <ed@iowacollegefoundation.org>
2020-06-26 11:12:08 -0500

    End of section 10

---
+++ DatingApp.API/Dtos/UserForUpdateDto.cs
@@ -0,0 +1,11 @@
+namespace DatingApp.API.Dtos
+{
+    public class UserForUpdateDto
+    {
+        public string Experiences { get; set; }
+        public string LookingFor { get; set; }
+        public string Major { get; set; }
+        public string Hometown { get; set; }
+        public string College { get; set; }
+    }
+}
